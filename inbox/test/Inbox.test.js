65884eb5b20940766536b0754d972e5d70eb664a
Rino Alfian <rino.alfian@detik.com>
2018-06-03 00:24:14 +0700

    add test scenario using mocha

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,3 +2,23 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async ()=> {
+    // get list dari semua account
+     accounts = await web3.eth.getAccounts();
+
+    //ambil akunya buat di deploy ke contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi There!']})
+        .send({ from: accounts[0], gas: '1000000'});
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
+});
