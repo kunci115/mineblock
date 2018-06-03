8a032ea89eab69cd46a0815755f99f2519e3842f
Rino Alfian <rino.alfian@detik.com>
2018-06-03 20:34:52 +0700

    p

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,24 +1,32 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'test message'
 
-beforeEach(async ()=> {
+beforeEach(async () => {
     // get list dari semua account
      accounts = await web3.eth.getAccounts();
 
     //ambil akunya buat di deploy ke contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi There!']})
-        .send({ from: accounts[0], gas: '1000000'});
-});
+        .deploy({ data: bytecode, arguments: ['test message'] })
+        .send({ from: accounts[0], gas: '1000000' });});
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'test message');
     });
 });
+
